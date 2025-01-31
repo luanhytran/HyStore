4a9d81759eb8cffdd743c4afbb2176b6f7e16933
luanhytran <hytranluan@gmail.com>
2025-01-31 11:49:26 +0700

    Init code

---
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -0,0 +1,32 @@
+import {Elements} from "@stripe/react-stripe-js";
+import CheckoutPage from "./CheckoutPage";
+import {loadStripe} from "@stripe/stripe-js";
+import { useState, useEffect } from 'react';
+import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { setBasket } from '../basket/basketSlice';
+
+const stripePromise = loadStripe(
+    import.meta.env.VITE_STRIPE_PK
+);
+
+export default function CheckoutWrapper() {
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        agent.Payments.createPaymentIntent()
+            .then(response => dispatch(setBasket(response)))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [dispatch]);
+
+    if (loading) return <LoadingComponent message='Loading checkout' />
+
+    return (
+        <Elements stripe={stripePromise}>
+            <CheckoutPage />
+        </Elements>
+    )
+}
